1cd6ef86ada1b45bbace9582f6531832ca5f8301
kyle mcarthur <teerzo@live.com.au>
2023-06-21 01:30:55 +1000

    and again

--- src/app/pvp/layout.tsx
+++ src/app/pvp/layout.tsx
@@ -4,9 +4,6 @@ export default function PVPLayout({ children, }: { children: React.ReactNode }) 
         <section>
             {/* Include shared UI here e.g. a header or sidebar */}
 
-
-
-            {/* <nav> PVP LAYOUT </nav> */}
             {children}
         </section>
     )
