7e478ff6ca5c724bb236b88e007b0a8c1c528608
Kyle <teerzo@live.com.au>
2025-02-24 03:37:47 +1100

    disabled additional jwt data

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -1,29 +1,49 @@
 'use client';
 
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "../utils/supabase/client";
 import { useState, useEffect, createContext } from "react";
 import { type Session } from '@supabase/supabase-js'
-import { jwtDecode } from 'jwt-decode'
+import { jwtDecode, JwtPayload } from 'jwt-decode'
 
-// import { ThemeProvider } from 'acme-theme';
-// import { AuthProvider } from 'acme-auth';
+// import { Enums } from "../types/supabase";
 
+// type Roles = Enums<'app_role'>;
+// type UserRole = Roles | null | undefined;
+// type ClinicId = string | null | undefined;
 
-export const SessionContext = createContext(null);
+// interface JWTRole extends JwtPayload {
+//     // user_role: Roles | null
+//     // clinic_id: string | null
+// }
+
+export type SessionValue = {
+    session: Session | null,
+    // userRole: UserRole,
+    // clinicId: ClinicId
+}
+
+export const SessionContext = createContext<SessionValue | null>(null);
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     const supabase = createClient();
     const [session, setSession] = useState<Session | null>(null)
-    const [userRole, setUserRole] = useState<string | null | undefined>(null);
+    // const [userRole, setUserRole] = useState<UserRole>(null);
+    // const [clinicId, setClinicId] = useState<ClinicId>(null);
 
     const decodeJWT = async (session: Session | null) => {
         if (session) {
-            const jwt = jwtDecode(session.access_token);
-            // console.log('jwt', jwt?.clinic_id, jwt?.user_role);
-            if (jwt && jwt?.user_role) {
-                return jwt.user_role; // custom jwt value
-            }
-            return null;
+            const jwt = jwtDecode<JWTRole>(session.access_token)
+            // console.log('jwt', jwt);
+            let decoded: JWTRole = {};
+            // let decoded: JWTRole = {
+            //     user_role: null,
+            //     clinic_id: null,
+            // }
+            // if (jwt) {
+            //     decoded.user_role = jwt?.user_role ? jwt.user_role : null;
+            //     decoded.clinic_id = jwt?.clinic_id ? jwt.clinic_id : null;
+            // }
+            return decoded;
         }
     }
 
@@ -31,33 +51,42 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         // BUG HERE - SIGNED_OUT is never called when user is logged out via supabase.auth.signOut()
         // https://github.com/supabase/auth-js/pull/854
 
-
         const subscription = supabase.auth.onAuthStateChange(
-            (event, session) => {
-                // console.log('event', event, session);
+            (event, newSession) => {
+                console.log('event', event, newSession, session);
                 setTimeout(async () => {
-
+                    console.log('event async', event, newSession, session);
+                    
+                    // $$$ compare session to user here
                     const { data: { user } } = await supabase.auth.getUser();
-                    // console.log('user', user);
+                    console.log('user', event, user);
 
-                    if (!user) {
-                        // console.log('session is invalid yeet and delete it');
-                        session = null;
-                        setSession(null)
-                        setUserRole(null)
+                    // if (!user) {
+                    //     // console.log('session is invalid yeet and delete it');
+                    //     // session = null;
+                    //     // setSession(null);
+                    //     // setUserRole(null);
+                    //     // setClinicId(null);
+                    // }
+
+                    // if (event === 'INITIAL_SESSION') {
+
+                    // }
+                    // else if (event === 'SIGNED_OUT') {
+                    //     setSession(null)
+                    // } else if (newSession) {
+                    //     setSession(newSession)
+                    // }
+
+                    if (newSession !== null) {
+                        // console.log('session', session);
+                        const decoded = await decodeJWT(newSession);
+                        // console.log('decoded', decoded);
+
+                        // setUserRole(decoded?.user_role);
+                        // setClinicId(decoded?.clinic_id);
                     }
-
-                    if (event === 'SIGNED_OUT') {
-                        setSession(null)
-                    } else if (session) {
-                        setSession(session)
-                    }
-
-                    const role = await decodeJWT(session);
-                    setUserRole(role);
-
                 }, 0);
-
 
                 // if (event === 'INITIAL_SESSION' && !user ) {
                 //     setSession(null)
@@ -66,14 +95,17 @@ export default function Providers({ children }: { children: React.ReactNode }) {
             })
 
         return () => {
-            if (subscription?.unsubscribe) {
-                subscription?.unsubscribe()
-            }
+            // $$$ unsubscribe doesnt exist?
+            // if (subscription?.unsubscribe) {
+            //     subscription?.unsubscribe()
+            // }
         }
-    }, [])
+    }, [supabase.auth])
+
 
     return (
-        <SessionContext.Provider value={{ session: session, userRole: userRole }}>
+        <SessionContext.Provider value={{ session: session }}>
+        {/* <SessionContext.Provider value={{ session: session, userRole: userRole, clinicId: clinicId }}> */}
             {/* <SessionContext.Provider value={session}> */}
             {children}
         </SessionContext.Provider>
